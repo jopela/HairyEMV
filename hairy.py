088280946ed187677131cd9fe70179001236b649
Jonathan Pelletier <jonathan.pelletier1@gmail.com>
2013-06-05 21:47:23 -0400

    adding doctests to the module

--- hairy.py
+++ hairy.py
@@ -1,34 +1,82 @@
 #!/usr/bin/python2
 
+CHAR_SET = set("0123456789abcdefABCDEF")
+
 import argparse
 
 def main():
 
-    parser = argparse.ArgumentParser('takes 3 values representing IAC or'\
-            ' CIAc and "unroll" them, for human readability')
+    parser = argparse.ArgumentParser()
 
     parser.add_argument(
             'decline',
-            help='value of the decline xIAC (e.g:FC0011AB00)'
+            help='value of the decline xIAC (e.g:FC0011AB00)',
             )
 
     parser.add_argument(
             'online',
-            help='value of the online xIAC (e.g:FC0011AB00)'
+            help='value of the online xIAC (e.g:FC0011AB00)',
             )
 
     parser.add_argument(
             'default',
-            help='value of the decline xIAC (e.g:FC0011AB00)'
+            help='value of the decline xIAC (e.g:FC0011AB00)',
             )
+
+    parser.add_argument(
+            '-t',
+            '--test',
+            help='run the doctests',
+            default=False,
+            action='store_true')
 
     args = parser.parse_args()
 
-    print "ARGUMEMTS"
-    print "decline:", args.decline
-    print "online:", args.online
-    print "default:", args.default
+    if args.test:
+        print "running doctests. You will see a report for failed tests."
+        import doctest
+        doctest.testmod()
+        print "Done running doctests ... quitting."
+        return
+
+    decline = args.decline
+    online = args.online
+    default = args.default
+
+    val_in = validate(decline, online, default)
+
     return
+
+def validate(decline, online, default):
+    """ Returns true if the decline, online and default command line parameters
+    are well formated and usable and False if this is not the case.
+    
+    Examples
+    ==============
+    >>> validate("0000000000","0000000000","0000000000")
+    True
+    >>> validate("0000000000","00","0000000000")
+    False
+    >>> validate("xxxxxxxxxx","0000000000","zzzzzzzzzz")
+    False
+    >>> validate("0011223344","5566778899","aaBbccDdEE")
+    True
+    >>> validate("0011223344","5566778899","aaBbffDdEE")
+    True
+    >>> validate("q011223344","5566778899","aaBbffDdEE")
+    False
+    
+    """
+
+    # All xIAC must have the same length.
+    l = [decline, online, default]
+    lengths_ok = len(set([len(i) for i in l])) == 1
+
+    # ALL xIAC must be HEX values.
+    l = set(decline+online+default)
+    in_char_set = len(l.difference(CHAR_SET)) == 0
+
+    return lengths_ok and in_char_set
 
 if __name__ == "__main__":
     main()
