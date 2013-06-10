6ff67fc3af789ca256031f088d58f58da8bf4fbb
Jonathan Pelletier <jonathan.pelletier1@gmail.com>
2013-06-09 23:46:39 -0400

    API definition tlv arser

---
+++ HairyEMV/tlv.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python2
+
+import argparse
+import doctest
+import util
+
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    # parameter for the TLV.
+    parser.add_argument(
+            'TLV',
+            help='TLV string (coded in hexadecimal).'
+            )
+
+    # doctest flag.
+    parser.add_argument(
+            '-t',
+            '--test',
+            help='run the doctests and exit.',
+            action='store_true'
+            )
+
+    args = parser.parse_args()
+
+    if args.test:
+        doctest.testmod()
+        return
+
+    print human(args.TLV)
+
+    return
+
+def head(tlv):
+    """ Takes a string containing one or more composite or primitive
+    ber-tlv encoded object and returns the first Tag-Lenght-Value triad
+    found.
+    
+    Example
+    =======
+
+    >>> head('840E315041592E5359532E4444463031A50E8801015F2D046672656E9F110101') 
+    '840E315041592E5359532E4444463031'
+    >>> head('840')
+    ''
+    
+    """
+    return "please implement me !"
+
+def tail(tlv):
+    """ Takes a string containing one or more composite or primitive ber-tlv
+    encoded object and returns everything but the first Tag-Length-Value triad
+    found.
+    
+    Example
+    =======
+
+    >>> tail('840E315041592E5359532E4444463031A50E8801015F2D046672656E9F110101')
+    'A50E8801015F2D046672656E9F110101'
+    ''
+    
+    """
+    return "please implement me!"
+
+def find(tag, tlv):
+    """ Takes tlv and search for tag, returning the Tag-Length-Value triad
+    associated with it if found. 
+    
+    Example
+    =======
+    >>> find('9F11','6F20840E315041592E5359532E4444463031A50E8801015F2D046672656E9F110101')
+    '9F110101'
+    """
+
+    return "please implement me!"
+
+def children(tlv):
+    """ Takes a ber-tlv encoded object and return the list of tlv data object
+    contained in it's value field.
+
+    Example
+    =======
+
+    >>> children('6f20840e315041592e5359532e4444463031a50e8801015f2d046672656e9f110101')
+    '840e315041592e5359532e4444463031a50e8801015f2d046672656e9f110101'
+
+    """
+
+    return "please implement me"
+
+def human(tlv):
+    """ Returns the human readable string for the TLV input. """
+    return "human:plese implement me"
+
+if __name__ == "__main__":
+    main()
+
